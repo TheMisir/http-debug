ea62eae445ea4e367951e2091b06f607b789dcef
Misir Jafarov <misir.ceferov@gmail.com>
2020-08-09 02:49:22 +0400

    Replaced http server with tiny http

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,43 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server, Version};
-use hyper::service::{make_service_fn, service_fn};
+use std::env;
+use tiny_http::{Server, Response};
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-  let uri = req.uri();
-  let method = req.method().as_str();
-  let version = match req.version() {
-    Version::HTTP_09 => "HTTP/0.9",
-    Version::HTTP_10 => "HTTP/1.0",
-    Version::HTTP_11 => "HTTP/1.1",
-    Version::HTTP_2 => "HTTP/2.0",
-    Version::HTTP_3 => "HTTP/3.0",
-    _ => "HTTP"
-  };
-
-  println!("{} {} {}", method, uri, version);
-
-  for (key, value) in req.headers() {
-    println!("{}: {}", key, value.to_str().unwrap());
-  }
-  
-  println!();
-  
-  Ok(Response::new("Hello, World".into()))
+fn get_port() -> u16 {
+  env::var("PORT")
+    .or(Ok("8080".to_string()))
+    .and_then(|s| s.parse::<u16>())
+    .or::<u16>(Ok(8080))
+    .unwrap()
 }
 
-#[tokio::main]
-async fn main() {
-  let port = std::env::var("PORT")
-    .or(Ok("8080".to_string()))
-    .and_then(|st| {
-      st.parse::<u16>()
-    })
-    .or::<u16>(Ok(8080))
-    .unwrap();
-  
-  let addr = SocketAddr::from(([127, 0, 0, 1], port));
-  let make_svc = make_service_fn(|_conn| async {
-    Ok::<_, Infallible>(service_fn(handle_request))
-  });
+fn main() {
+  let port = get_port();
+  let server = Server::http(format!("0.0.0.0:{}", port)).unwrap();
 
-  let server = Server::bind(&addr).serve(make_svc);
+  println!("Server is running at {}", server.server_addr());
 
-  println!("Server is running at http://127.0.0.1:{}", port);
+  for request in server.incoming_requests() {
+    // convert headers to key: value format
+    let headers: Vec<_> = request.headers()
+      .into_iter()
+      .map(|header| format!("{}: {}",
+        header.field.as_str(),
+        header.value.as_str()
+      ))
+      .collect();
 
-  if let Err(e) = server.await {
-    eprintln!("server error: {}", e);
+    // format body
+    let body = &format!("{} {} HTTP/{}\n{}\n",
+      request.method(),
+      request.url(),
+      request.http_version(),
+      headers.join("\n")
+    );
+
+    // create response from body
+    let response = Response::from_string(body);
+
+    // print and respond
+    println!("{}", body);
+    request.respond(response).unwrap();
   }
 }
